15657c61bbca1e7e3b4d0ef8c9cbfbfcb9280d0e
Kenneth Laqui <kennethlaqui@yahoo.com>
2021-02-01 15:55:12 +0800

    Refactor router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../App.vue'
+import App from '../App.vue'
 import Form from '../components/Form.vue'
 import Appointments from '../components/Appointments.vue'
 
@@ -10,20 +10,10 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
-  },
-  {
-    path: '/appointment/form',
-    name: 'AppointmentForm',
-    component: Form
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: Form,
+    meta: {
+      layout: App
+    }
   },
   {
     path: '/appointments',
