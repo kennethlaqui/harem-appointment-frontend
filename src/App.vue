00cb4e41a420196cb8554cd7f1ea656a28815d63
Kenneth Laqui <kennethlaqui@yahoo.com>
2021-02-03 11:30:19 +0800

    Update App.vue format

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,6 @@
       hide-on-scroll
     >
 
-      <!-- <v-app-bar-title>Virtual Logic Inc.</v-app-bar-title> -->
-
       <v-spacer />
 
       <!-- twitter -->
@@ -61,10 +59,20 @@
     </v-app-bar>
 
     <v-main>
-      <v-row align="center" justify="center">
-        <v-col align="center" justify="center">
 
-          <v-img class="mx-16"
+      <!-- logo -->
+      <v-row
+        align="center"
+        justify="center"
+      >
+
+        <v-col
+          align="center"
+          justify="center"
+        >
+
+          <v-img
+            class="mx-16"
             alt="strip"
             contain
             :src="images.company"
@@ -76,45 +84,76 @@
 
       </v-row>
 
-      <v-row align="center" justify="center" class="mt-n16">
-         <v-col align="center" justify="center">
+      <!-- strip description -->
+      <!-- 1st descript -->
+      <v-row
+        align="center"
+        justify="center"
+        class="mt-n16"
+      >
+
+        <v-col
+          align="center"
+          justify="center"
+        >
+
           <h1 class="stripH1">Hey Strip Mates!</h1>
-         </v-col>
+
+        </v-col>
+
       </v-row>
 
-      <v-row align="center" justify="center">
-         <v-col md="6">
+      <!-- 2nd descript -->
+      <v-row
+        align="center"
+        justify="center"
+      >
+
+        <v-col md="6">
+
           <h1 class="text-justify.text-center stripH1"> Welcome to the Strip: Ministry of Waxing, the Global Authority in Hair Removal with 48 parlours in 12 major capital cities worldwide.</h1>
-         </v-col>
+
+        </v-col>
+
       </v-row>
+      <!-- end of strip description -->
+
+      <!-- browhaus description -->
+      <!-- end of browhaus description -->
 
       <v-spacer />
 
+      <!-- if using vue-router -->
       <router-view />
 
     </v-main>
+
   </v-app>
+
 </template>
 
 <script>
 
 export default {
   name: 'App',
-  data: () => ({
-    style: false,
-    images: {
-      company: require('@/assets/strip.jpg')
+  data () {
+    return {
+      style: false,
+      images: {
+        company: require('@/assets/strip.jpg')
+      }
     }
-  })
+  }
 }
 </script>
 
 <style lang ="scss">
-  @import url('http://fonts.cdnfonts.com/css/gotham');
-    .stripH1{
-      font-family: 'Gotham Thin', sans-serif;
-      text-align: center;
-      color:#4A148C;
-    }
+@import url('http://fonts.cdnfonts.com/css/gotham');
+
+  .stripH1{
+    font-family: 'Gotham Thin', sans-serif;
+    text-align: center;
+    color:#4A148C;
+  }
 
 </style>
